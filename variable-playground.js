714f87dcc42d07af2125fa566457820999bf7885
Harshala Shirude <harshalas@winjit.com>
2016-06-02 12:47:52 +0530

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+/*var person = {
+	name: "Harshala",
+	age: 21
+}
+
+function updatePerson(person)
+{
+	person = {
+		name: "Andrew",
+		age: 24
+	}
+	person.age=24;
+}
+
+updatePerson(person);
+console.log(person);*/
+
+var grades = [15,88];
+
+function addGrades(gradesArr)
+{
+	//grades = [11,22,33];
+	debugger;
+	gradesArr.push(55);
+
+}
+
+addGrades(grades);
+console.log(grades);
