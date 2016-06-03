bcc5663c33121b1153c647343ce23f3d03544ea2
Harshala Shirude <harshalas@winjit.com>
2016-06-03 16:54:04 +0530

    Convert sequelize to postgres on heroku

--- db.js
+++ db.js
@@ -1,5 +1,16 @@
 var Sequelize = require("Sequelize");
-var sequelize = new Sequelize(undefined, undefined, undefined,{
+var sequelize;
+var env = Process.env.NODE_ENV || "development";
+
+if(env==="production")
+{
+	sequelize = new Sequelize(Process.env.DATABASE_URL,{
+		dialect: "postgres"
+	});
+}
+else
+{
+	sequelize = new Sequelize(undefined, undefined, undefined,{
 	dialect:"sqlite",
 	storage: __dirname + "/data/dev-todo-api.sqlite"
 });
