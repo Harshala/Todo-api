758817313ff9e3172ac301cdb0e9cb980488fd66
Harshala Shirude <harshalas@winjit.com>
2016-06-03 15:34:28 +0530

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var _ = require('underscore');
+var db = require("./db.js");
 var todoIdNext = 1;
 var bodyParser = require('body-parser');
 
@@ -83,7 +84,7 @@ app.post('/todos', function(req, res) {
 
 	var body = _.pick(req.body, "description", "complete");
 
-	if (!_.isBoolean(body.complete) || !_.isString(body.description) || body.description.trim().length === 0) {
+	/*if (!_.isBoolean(body.complete) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
 
@@ -91,7 +92,13 @@ app.post('/todos', function(req, res) {
 
 	body.id = todoIdNext++;
 	todos.push(body);
-	res.json(body).send();
+	res.json(body).send();*/
+
+	db.todo.create(body).then(function(todo){		
+			res.json(todo.toJSON());
+		},function(e){
+			res.status(400).json(e);
+		});
 });
 
 app.delete('/todos/:id', function(req, res) {
@@ -137,6 +144,9 @@ app.put('/todos/:id', function(req, res) {
 	}
 });
 
-app.listen(PORT, function() {
-	console.log('Server running at port ' + PORT);
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function() {
+		console.log('Server running at port ' + PORT);
+	});
 });
+
